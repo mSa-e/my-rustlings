11a317ed5db4a411194ce78935b0c060280ece01
/ <itsmemarwan9709@gmail.com>
2026-09-25 09:04:19 +0300

    solve(01_variables): complete variables5 through variables6
         - variables5: The shadowing feature providing data type frexiability
         - variables6: const constraint

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,5 +1,5 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+const NUMBER: i32 = 3; // With cost we must provide the data type
 
 fn main() {
     println!("Number: {NUMBER}");
